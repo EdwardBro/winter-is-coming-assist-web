1d4f88575e5013eb4d390f4ec8aee353879d5c7e
EdBro <edbr.dev@gmail.com>
2025-03-20 16:56:33 +0200

    FAQ page created

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FAQPage() {
+  return (
+    <div className="flex container justify-center mx-auto p-4">
+      <p className="text-xl font-semibold">FAQ</p>
+    </div>
+  );
+}
+
+export default FAQPage;
