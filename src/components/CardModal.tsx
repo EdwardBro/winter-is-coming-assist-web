424b5a3782ee423c532d8e8a2fa6445caa0adbdb
EdBro <edbr.dev@gmail.com>
2025-04-10 13:04:46 +0300

    Card modal component change

--- src/components/CardModal.tsx
+++ src/components/CardModal.tsx
@@ -3,6 +3,7 @@
 import { SimpleCard } from "@/types";
 import { X } from "lucide-react";
 import Image from "next/image";
+import { useEffect } from "react";
 
 interface CardModalProps {
   card: SimpleCard;
@@ -11,6 +12,13 @@ interface CardModalProps {
 
 // Modal component for displaying a selected card.
 const CardModal: React.FC<CardModalProps> = ({ card, onClose }) => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
     <div
       className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50 bg-black/70 backdrop-blur-sm transition-opacity duration-300"
@@ -19,28 +27,45 @@ const CardModal: React.FC<CardModalProps> = ({ card, onClose }) => {
       role="dialog"
     >
       <div
-        className="relative w-full max-w-xl bg-white rounded-2xl shadow-xl overflow-hidden transform transition-all duration-300 scale-100"
+        className="overflow-hidden relative w-[90vw] max-w-3xl max-h-[90vh] bg-blue-200 rounded-2xl shadow-lg transform transition-all duration-300 scale-100"
         onClick={(e) => e.stopPropagation()}
       >
         <button
           onClick={onClose}
           aria-label="Закрыть"
-          className="absolute top-3 right-3 p-2 bg-gray-200 rounded-full hover:bg-gray-300 transition"
+          className="fixed md:absolute top-4 right-4 z-50 p-2 bg-gray-200 rounded-full hover:bg-gray-400 transition opacity-90"
         >
           <X className="w-5 h-5 text-gray-600" />
         </button>
-        <Image
-          src={card.image}
-          alt={card.title}
-          className="w-full max-h-[60vh] object-cover"
-          onClick={(e) => e.stopPropagation()}
-          loading="lazy"
-        />
-        <div className="p-4">
-          <h2 className="text-2xl font-bold mb-2 text-gray-700">
-            {card.title}
-          </h2>
-          <p className="text-gray-700">{card.description}</p>
+
+        <div className="overflow-y-auto max-h-[90vh] pt-2 bg-gray-100">
+          <div className="w-full relative aspect-[3/4]">
+            <Image
+              src={card.image}
+              alt={card.title}
+              width={600}
+              height={800}
+              className="object-cover"
+              /*            onClick={(e) => e.stopPropagation()}*/
+              loading="lazy"
+            />
+          </div>
+
+          <div className="p-4 space-y-4">
+            <h2 className="text-2xl font-bold text-gray-800">{card.title}</h2>
+            <p className="text-gray-700 whitespace-pre-line">
+              {card.description}
+            </p>
+
+            <Image
+              src={`/assets/gifs/${card.id}.gif`}
+              alt={`Анимация для ${card.title}`}
+              width={800}
+              height={600}
+              className="w-full object-contain rounded-xl"
+              /*              loading="lazy"*/
+            />
+          </div>
         </div>
       </div>
     </div>
